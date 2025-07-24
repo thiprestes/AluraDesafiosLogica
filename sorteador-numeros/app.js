6257aea65437243025b5eea1b4e0c439a7363195
Thiago Prestes <thiprestes_@hotmail.com>
2025-07-23 22:44:00 -0300

    Added errors logic and restart function

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,51 @@
+let restartButtonElement = document.getElementById("btn-reiniciar");
+let quantityElement = document.getElementById("quantidade");
+let firstElement = document.getElementById("de");
+let lastElement = document.getElementById("ate");
+let messageElement = document.getElementById("resultado");
+
+function draw() {
+    let quantityNumber = document.getElementById("quantidade").value;
+    let firstNumber = document.getElementById("de").value;
+    let lastNumber = document.getElementById("ate").value;
+
+    if (checkParams(quantityNumber, firstNumber, lastNumber)) {
+    //se estiver tudo certo  
+    ajustRestartButton(true);  
+    };
+    ajustRestartButton(false);
+}
+
+function restart() {
+    ajustRestartButton(true);
+    quantityElement.value = '';
+    firstElement.value = '';
+    lastElement.value = '';
+    let teste = messageElement.getElementsByClassName("text__paragrafo")
+    teste.innerHTML = 'testes';
+}
+
+function checkParams(quantity, first, last) {
+    let message = '';
+    let possibilities = last - first;
+
+    if (possibilities < quantity ){
+        message = message + "\n Quantidade informada maior que as possibilidades"
+    }
+
+    if (last < first) {
+        message = message + "\n Valores de inicio e fim incorretos"
+    }
+
+    if (message != '') {
+        alert(message);
+        return false;
+    };
+
+    return true;
+}
+
+function ajustRestartButton(buttonDisabled) {
+    restartButtonElement.className = "container__botao";
+    restartButtonElement.disabled = buttonDisabled;
+}
