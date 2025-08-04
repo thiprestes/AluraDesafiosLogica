d3f0b56da2543168de686b5e207215ad4220dc32
Thiago Prestes <thiprestes_@hotmail.com>
2025-08-04 08:37:05 -0300

    added function to change label and Img
    Added a funtion to change label;
    Added a function to change class of the Img.

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -1,12 +1,30 @@
 function changeStatus(game) {
-    let divElement = document.getElementById(`game-${game}`);
-    let aElement = divElement.getElementsByTagName('a');
+    let liElement = document.getElementById(`game-${game}`);
+    
+    changeLabel(liElement);
+    
+    changeImg(liElement);
+    
+}
+
+function changeLabel(Element) {
+    let aElement = Element.getElementsByTagName('a');
 
     if (aElement.item(0).classList.contains('dashboard__item__button--return')) {
         aElement.item(0).classList.remove('dashboard__item__button--return');
         aElement.item(0).innerHTML = 'Alugar';
     } else {
         aElement.item(0).classList.add('dashboard__item__button--return');
-        aElement.item(0).innerHTML = 'Devolver';    
-    }
+        aElement.item(0).innerHTML = 'Devolver';  
+    };
+}
+
+function changeImg(Element) {
+    let divElement = Element.getElementsByTagName('div');
+
+    if (divElement.item(0).classList.contains('dashboard__item__img--rented')) {
+        divElement.item(0).classList.remove('dashboard__item__img--rented');
+    } else {
+        divElement.item(0).classList.add('dashboard__item__img--rented');
+    };
 }
