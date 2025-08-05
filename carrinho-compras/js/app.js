2d59f8b905d1a5a74dfc26d8791bfc7153ee1570
Thiago Prestes <thiprestes_@hotmail.com>
2025-08-04 21:55:59 -0300

    remove ifs and simplify code
    remove some ifs and simplify the function using split to get the item's name and value.

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -5,25 +5,16 @@ let cartTotal = document.getElementById('valor-total');
 function addItem() {
     let quantity = document.getElementById('quantidade').value;
     let item = document.getElementById('produto').value;
+    let nameItem = item.split('-')[0];
+    let valueItem = item.split('R$')[1];
     
     if (quantity == '') {
         alert('Informe uma quantidade valida!');
     } else {
-        if (item == 'Fone de ouvido - R$100') {
-            cartItems.innerHTML = cartItems.innerHTML + `<section class="carrinho__produtos__produto"> <span class="texto-azul">${quantity}x</span> Fone de ouvido <span class="texto-azul">R$100</span> </section>`;
-            totalCartValue = totalCartValue + (quantity * 100); 
-        } else if (item == 'Celular - R$1400') {
-            cartItems.innerHTML = cartItems.innerHTML + `<section class="carrinho__produtos__produto"> <span class="texto-azul">${quantity}x</span> Celular <span class="texto-azul">R$1400</span> </section>`; 
-            totalCartValue = totalCartValue + (quantity * 1400);
-        } else if (item == 'Oculus VR - R$5000'){
-            cartItems.innerHTML = cartItems.innerHTML + `<section class="carrinho__produtos__produto"> <span class="texto-azul">${quantity}x</span> Oculus VR <span class="texto-azul">R$5000</span> </section>`; 
-            totalCartValue = totalCartValue + (quantity * 5000);
-        } else {
-            alert('Item informado não existe na lista de itens.')
-        }
-    updateTotalCart();
+        cartItems.innerHTML = cartItems.innerHTML + `<section class="carrinho__produtos__produto"> <span class="texto-azul">${quantity}x</span> ${nameItem} <span class="texto-azul">R$${valueItem}</span> </section>`;
+        totalCartValue = totalCartValue + (quantity * valueItem); 
+        updateTotalCart();
     }
-
 }
 
 function clearCart(){
