50052cdc6fa0fc69cc5aaa3f55d98492470bcb18
Thiago Prestes <thiprestes_@hotmail.com>
2025-08-05 22:27:06 -0300

    Add validations of number of friends and repeated friends
    Added a validation to check how much friends have on the list and set the minimum number of friends is 4;
    Added a validation to check if the friend is already on the list.

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,26 +1,36 @@
 let friends = [];
 
 function add() {
-    if (document.getElementById('nome-amigo').value != '') {
-        friends.push(document.getElementById('nome-amigo').value);
+    let friendName = document.getElementById('nome-amigo').value;
+    if (friendName != '') {
+        if (friends.indexOf(friendName) == -1 ) {
+            friends.push(document.getElementById('nome-amigo').value);
+        } else {
+            alert('Amigo já adicionado na lista!');
+        }
     }
     updateIncludedFriends();
     document.getElementById('nome-amigo').value = '';
 };
 
 function draw() {
-    let friendsLeft = friends.slice();
-    let i = 0;
-    while (i < friends.length) {
-        let friend = friends[i];
-        let friendSelected = parseInt(Math.random() * friendsLeft.length);
-        let friendNameSelected = friendsLeft[friendSelected];
-        if (friend != friendNameSelected) {
-            document.getElementById('lista-sorteio').innerHTML = document.getElementById('lista-sorteio').innerHTML + `${friend} -> ${friendNameSelected} <br>`;
-            friendsLeft.splice(friendSelected, 1);
-            i++
-        }
-    };      
+    if (friends.length > 4) {
+        let friendsLeft = friends.slice();
+        let i = 0;
+        while (i < friends.length) {
+            let friend = friends[i];
+            let friendSelected = parseInt(Math.random() * friendsLeft.length);
+            let friendNameSelected = friendsLeft[friendSelected];
+            if (friend != friendNameSelected) {
+                document.getElementById('lista-sorteio').innerHTML = document.getElementById('lista-sorteio').innerHTML + `${friend} -> ${friendNameSelected} <br>`;
+                friendsLeft.splice(friendSelected, 1);
+                i++
+            }
+        };         
+    } else {
+        alert('O número minimo de participantes é 4!');
+    }
+     
 };
 
 function restart() {
